d4082729caf8bf5004c548cf9be1302ec8157d73
villalb4 <villalb4angel@gmail.com>
2022-07-11 15:09:57 -0300

    Trabajando en la search bar

--- client/src/Components/Header/SearchBar/SearchBar.jsx
+++ client/src/Components/Header/SearchBar/SearchBar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import './SearchBar.css'
+import './SearchBar.css';
+import search_icon from "../../../images/search-icon.svg";
+import { useSelector } from "react-redux";
 
 function SearchBar() {
+
+  const dogs = useSelector(state => state.dogs)
+
   return(
-    <div>
-      <input className="searchBar" type="text" placeholder="Buscar" />
+    <div className="searchBar_Container">
+      <div className="divInput_SearchBar">
+        <img className="searchIcon" src={search_icon} alt="serach" />
+        <input className="searchBar" type="text" placeholder="Buscar" />
+      </div>
+      <div className="divSearchBar_Results">
+
+      </div>
     </div>
   )
 }
