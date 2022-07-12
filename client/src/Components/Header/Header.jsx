dc915b211e6d6bea33d7a3e036a72e2845214172
villalb4 <villalb4angel@gmail.com>
2022-07-12 18:06:09 -0300

    Update header

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -3,13 +3,14 @@ import {Link, NavLink} from 'react-router-dom'
 import SearchBar from './SearchBar/SearchBar';
 import './Header.css';
 
+
 function Header() {
   return(
     <div className="header">
       <div className="header_cont">
         <Link to="/home" className="logo"><h1 className="logo">DOGS</h1></Link>
         <div className="nav">
-          <NavLink to="#" className="about">About</NavLink>
+          <NavLink to="#" className="about">GitHub</NavLink>
           <SearchBar />
         </div>
       </div>
