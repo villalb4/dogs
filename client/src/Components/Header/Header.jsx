28af4b75a144239c47ca1c4393ab2b3229f4dcef
villalb4 <villalb4angel@gmail.com>
2022-07-19 17:39:50 -0300

    Header responsive

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import SearchBar from './SearchBar/SearchBar';
 import './Header.css';
+import menu from '../../images/menu.svg';
 
 
 function Header() {
   return(
     <div className="header">
       <div className="header_cont">
-        <Link to="/home" className="logo"><h1 className="logo">DOGS</h1></Link>
+        <div className='div_responsive_header'>
+          <Link to="/home" className="logo"><h1 className="logo">DOGS</h1></Link>
+          <img className='menu' src={menu} alt="menu" />
+        </div>
         <div className="nav">
           <a href="https://github.com/villalb4/Dogs" target="_balck" className="about">GitHub</a>
           {/* <NavLink to="#" className="about">GitHub</NavLink> */}
